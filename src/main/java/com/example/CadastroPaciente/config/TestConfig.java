0ec07799d1d0fb96263d19046b0c36653fa4bbed
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-08 16:58:45 -0300

    Test profile

---
+++ src/main/java/com/example/CadastroPaciente/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.example.CadastroPaciente.config;
+
+import com.example.CadastroPaciente.entities.Paciente;
+import com.example.CadastroPaciente.repositories.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private PacienteRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Paciente paciente = new Paciente(null, "Joseph", "0949043409", 1);
+
+        repository.save(paciente);
+    }
+}
